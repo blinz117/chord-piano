2217bc0212c94ec025eebd93c57828a9f834c130
Bryan Lindsey <blinz117@gmail.com>
2021-08-28 09:32:11 -0500

    Animate piano key height

--- src/components/PianoKey.js
+++ src/components/PianoKey.js
@@ -1,13 +1,22 @@
 import { Paper, Typography } from "@material-ui/core";
+import { useState } from "react";
 import "./PianoKey.css";
 
 function PianoKey({ label, keyPressed, keyReleased }) {
+  const [isPressed, setIsPressed] = useState(false);
+
   return (
     <Paper
       className="piano-key"
-      elevation={2}
-      onPointerDown={keyPressed}
-      onPointerUp={keyReleased}
+      elevation={isPressed ? 1 : 3}
+      onPointerDown={() => {
+        setIsPressed(true);
+        keyPressed();
+      }}
+      onPointerUp={() => {
+        setIsPressed(false);
+        keyReleased();
+      }}
     >
       <Typography className="piano-key-label">{label}</Typography>
     </Paper>
